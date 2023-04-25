45238a93336f0f2e340965264e200ba17c286726
sudipto <cg.sudipc@gmail.com>
2023-04-25 23:52:33 +0530

    JAVA IO RECAP

--- src/com/example/io/IOPgms.java
+++ src/com/example/io/IOPgms.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
@@ -14,6 +15,13 @@ import java.util.TreeSet;
 class PathNotFoundException extends Exception{
 	public PathNotFoundException(String message) {
 		super(message);
+	}
+}
+
+class StringComparator implements Comparator<String> {
+	@Override
+	public int compare(String o1, String o2) {
+		return o2.compareTo(o1);
 	}
 }
 
@@ -44,8 +52,10 @@ public class IOPgms {
 							for(String s : words) {
 								wordList.add(s);
 								wordSet.add(s);
-								Collections.sort(wordList);
-								
+								/*sort in List*/
+								//Collections.sort(wordList);
+								/**reverse sort in list*/
+								Collections.sort(wordList, new StringComparator());
 							}
 						}
 						System.out.println("sorted in List with duplicates= " + wordList);
@@ -58,10 +68,45 @@ public class IOPgms {
 		}
 	}
 	
-	public static void main(String[] args) {
+	/**num of occurances of a word in a file*/
+	public static void countOccuranceOfWord(String path, String str) throws PathNotFoundException {
+		if(path == null) {
+			throw new PathNotFoundException("Path can not be null");
+		}
+		File f = new File(path);
+		if(f.exists() && f.isDirectory()) {
+			File[] fa = f.listFiles();
+			for(File file : fa) {
+				if(file.exists() && file.isFile() && file.getName().endsWith(".txt")) {
+					BufferedReader br = null;
+					try {
+						br = new BufferedReader(new FileReader(file));
+						String line;
+						int count = 0;
+						int pos = 0;
+						while((line = br.readLine()) != null) {
+							while((pos = line.indexOf(str, pos)) != -1) {
+								++count;
+								++pos;
+							}
+						}
+						System.out.println("count of the word: " + count);
+					}
+					catch(Exception e) {
+						
+					}
+				}
+			}
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) throws PathNotFoundException {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a path");
 		String path = sc.nextLine();
-		wordSort(path);
+		//wordSort(path);
+		countOccuranceOfWord(path, "an");
 	}
 }
